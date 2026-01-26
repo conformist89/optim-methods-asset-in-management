e722f48035e7ee52807167ffc623aae7ea4d2280
conformist89 <olhalavoryk@gmail.com>
2026-01-26 11:24:22 +0100

    File with code that makes optimization

---
+++ optimizer.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize 
+
+
+def get_risk(x,cov_matrix): 
+    # this function is responsible for returning variance of portfolio; in this case variance is used as risk measure
+    x = np.asarray(x, float)
+    return float(x.T @ cov_matrix @ x)
+
+def mean_variance_opt(mu, cov_matrix, target_return, short_allowed=True):
+    # this function performes mean-variance optimization with minimization 
+    # of variance that used as risk measure and maximization of return for 
+    # targeted variance
+    mu = np.asarray(mu, float).reshape(-1)
+    cov_matrix = np.asarray(cov_matrix, float)
+    n = mu.size
+    ones = np.ones(n)
+
+    # Initial guess: equal weights
+    x0 = np.full(n, 1.0 / n)
+
+    # Constraints:
+    # 1) mu^T x - target_return >= 0
+    # 2) 1^T x - 1 = 0
+    constraints = [
+        {"type": "ineq", "fun": lambda x: float(mu @ x) - target_return},
+        {"type": "eq",   "fun": lambda x: float(ones @ x) - 1.0},
+    ]
+
+
+    # Bounds (optional): long-only vs allow shorting
+    if short_allowed:
+        bounds = None
+    else:
+        bounds = [(0.0, 1.0)] * n
+
+    result = minimize(
+        fun=get_risk,
+        x0=x0,
+        args=(cov_matrix,),
+        method="SLSQP",
+        constraints=constraints,
+        bounds=bounds,
+        options={"ftol": 1e-12, "maxiter": 10_000}
+)
+
+    if not result.success:
+        raise RuntimeError(f"Optimization failed: {result.message}")
+
+    x_star = result.x
+    var_star = get_risk(x_star, cov_matrix)
+    ret_star = float(mu @ x_star)
+
+    return x_star, var_star, ret_star, result
